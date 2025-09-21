a615474954fb27140fbb35a2fc42ca839882bb90
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-09-21 22:52:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Suspense } from "react";
+import "./App.css";
+
+import AvailablePlayers from "./components/AvailablePlayers/AvailablePlayers";
+import NavBar from "./components/NavBar/NavBar";
+import SelectedPlayers from "./components/SelectedPlayers/SelectedPlayers";
+
+const fetchPlayers = async () => {
+  const res = await fetch("/players.json");
+  return res.json();
+};
+
+function App() {
+  const playersPromise = fetchPlayers();
+  return (
+    <>
+      <NavBar></NavBar>
+
+      <Suspense
+        fallback={<span className="loading loading-spinner loading-xl"></span>}
+      >
+        <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
+      </Suspense>
+
+      <SelectedPlayers></SelectedPlayers>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectedPlayers = () => {
+  return <div>Selected</div>;
+};
+
+export default SelectedPlayers;
